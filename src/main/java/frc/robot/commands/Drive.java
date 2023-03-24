be03f2fc09d04f837616aea05991f763eb451b20
Ace-h121 <Chathaway121@gmail.com>
2023-03-24 14:42:16 -0400

    Inital Commit

---
+++ src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,82 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.ControllerConstants;
+import frc.robot.subsystems.Drivebase;
+
+public class Drive extends CommandBase {
+
+  Drivebase drivebase;
+
+  XboxController driver;
+
+  double throttle;
+  double turn;
+
+  boolean brake;
+
+  public Drive(Drivebase drivebase) {
+
+    drivebase = this.drivebase;
+
+    driver = RobotContainer.getDriverController();
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drivebase);
+    
+  }
+
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+  }
+
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    throttle = driver.getLeftY();
+    turn = driver.getRightX();
+    brake = driver.getRightBumper();
+
+    if(brake){
+      drivebase.drive(0,0);
+    }
+
+    if(throttle > ControllerConstants.AXIS_THRESHOLD || throttle < ControllerConstants.NEGITIVE_AXIS_THRESHOLD){
+      if(turn > ControllerConstants.AXIS_THRESHOLD || turn < ControllerConstants.NEGITIVE_AXIS_THRESHOLD){
+        drivebase.drive(throttle, turn);
+      }
+      else{
+        drivebase.drive(throttle, 0);
+      }
+    }
+
+    if(turn > ControllerConstants.AXIS_THRESHOLD || turn < ControllerConstants.NEGITIVE_AXIS_THRESHOLD){
+      drivebase.drive(0, turn);
+    }
+
+
+  }
+
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -0,0 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+//vender library
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+//wpi/first imports
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+//imports from constant
+import frc.robot.Constants.MotorConstants;
+
+public class Drivebase extends SubsystemBase {
+  AHRS Gyro = new AHRS(I2C.Port.kMXP);
+
+  //Throttle Modules
+  CANSparkMax frontRightThrottle;
+  CANSparkMax frontLeftThrottle;
+  CANSparkMax backRightThrottle;
+  CANSparkMax backLeftThrottle;
+
+  //turning modules  
+  CANSparkMax frontRightTurn;
+  CANSparkMax frontLeftTurn;
+  CANSparkMax backRightTurn;
+  CANSparkMax backLeftTurn;
+
+  //motor groups
+  MotorControllerGroup ThrottleGroup;
+  MotorControllerGroup TurnGroup;
+
+  /** Creates a new Drivebase. */
+  public Drivebase() {
+
+    //defining the throttle motors
+    frontRightThrottle  = new CANSparkMax(MotorConstants.FRONT_RIGHT_THROTTLE, MotorType.kBrushless);
+    frontLeftThrottle   = new CANSparkMax(MotorConstants.FRONT_LEFT_THROTTLE, MotorType.kBrushless);
+    backRightThrottle   = new CANSparkMax(MotorConstants.BACK_RIGHT_THROTTLE, MotorType.kBrushless);
+    backLeftThrottle    = new CANSparkMax(MotorConstants.BACK_LEFT_THROTTLE, MotorType.kBrushless);
+
+    //defining the turn motors
+    frontRightTurn      = new CANSparkMax(MotorConstants.FRONT_RIGHT_TURN, MotorType.kBrushless);
+    frontLeftTurn       = new CANSparkMax(MotorConstants.FRONT_LEFT_TURN, MotorType.kBrushless);
+    backRightTurn       = new CANSparkMax(MotorConstants.BACK_RIGHT_TURN, MotorType.kBrushless);
+    backLeftTurn        = new CANSparkMax(MotorConstants.BACK_LEFT_TURN, MotorType.kBrushless);
+
+    //grouping throttle and turn motors
+    ThrottleGroup       = new MotorControllerGroup(backLeftThrottle, frontLeftThrottle, frontRightThrottle, backRightThrottle);
+    TurnGroup           = new MotorControllerGroup(backLeftTurn, frontLeftTurn, frontRightTurn, backRightTurn);
+  }
+
+  //setting up the drive method
+  public void drive(double speedInput, double turnInput){
+    ThrottleGroup.set(speedInput);
+    TurnGroup.set(turnInput);
+  }
+
+  //gyro methods
+  public void resetGyro(){
+    Gyro.reset();
+  }
+
+  public double getAngle(){
+    return Gyro.getAngle();
+  }
+
+  public AHRS getGyro(){
+    return Gyro;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    
+  }
+}
